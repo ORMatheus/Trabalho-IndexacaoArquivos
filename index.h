e5cc0f96ffc2760040c3888fcd8e9cbafb28b180
Matheus <Matheus.rdeoliveira77@gmail.com>
2025-11-23 13:54:40 -0300

    textProcessor ok

--- index.h
+++ index.h
@@ -17,3 +17,33 @@ using std::string;
 using std::vector;
 using std::unordered_map;
 using std::unordered_set;
+
+
+ //TEXT PROCESSOR
+//  Classe responsável por:
+//   - Transformar texto em minúsculas
+//   - Remover pontuação
+//   - Separar em palavras
+//   - Remover stopwords
+//   Isso garante que todas as palavras sejam tratadas de maneira igual.
+
+class TextProcessor{
+    private:
+    unordered_set<string> stopwords ; //conjunto de palavras para ser ignoradas
+
+    public:
+    TextProcessor(const string & stopwordsFile); //construtor recebe o nome do arquivo stopwords
+
+    //funções auxiliares
+    string minuscula(const string&s);
+    string removerPontuacao(const string&s);
+    vector <string> separarPalavras(const string &s);
+
+    //função principal para fazer o pre processamento
+    vector<string > processarLinha(const string& linha);
+    
+
+};
+
+
+#endif
